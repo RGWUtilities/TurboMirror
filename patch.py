0e1324a37979e85554ee3405709e439de73a257b
RGWUtilities <rgoldingwebb@gmail.com>
2026-05-11 07:58:47 +0100

    Make it more "school friendly"

--- patch.py
+++ patch.py
@@ -19,6 +19,24 @@ for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
   with open(path, 'w') as f:
     f.write(contents)
 
+# Open the main file
+with open('scratch-gui/build/index.html', 'r') as f:
+    content = f.read()
+
+# 1. Change the Tab Title
+content = content.replace('<title>TurboWarp</title>', '<title>OurCloud.buzz</title>')
+
+# 2. Change the Meta Description (what shows in history/search)
+content = content.replace('A high speed Scratch mod', 'A faster project version that makes learning for kids better')
+
+# 3. Global replacement for any other mentions in the HTML
+content = content.replace('TurboWarp', 'OurCloud.buzz')
+
+# Save the changes back to the file
+with open('scratch-gui/build/index.html', 'w') as f:
+    f.write(content)
+
+
 os.remove('scratch-gui/build/sw.js')
 os.remove('scratch-gui/build/manifest.webmanifest')
 os.remove('scratch-gui/build/fullscreen.html')
